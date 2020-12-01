b9cfaa8e0737a4270491a2ac668c3e0931913a7f
PlVasilev <pvvasilev2013@gmail.com>
2020-12-01 21:14:42 +0200

    Client edited angular.json and added environment.dev.ts; K8S edit development.yml

---
+++ Client/Seller.Client/src/environments/environment.dev.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: true,
+    identityApiUrl: "http://localhost:5003/",
+    offersApiUrl: "http://localhost:5005/",
+    listingsApiUrl: "http://localhost:5001/",
+    messagesApiUrl: "http://localhost:5007/",
+    listingGatewayApiUrl: "http://localhost:5009/",
+  };
