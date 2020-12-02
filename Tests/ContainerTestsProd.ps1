3cec23131f969c9f08e5e14a9b7b3e44d185e189
PlVasilev <pvvasilev2013@gmail.com>
2020-12-02 22:35:31 +0200

    Tests/ContainerTestsProd.ps1 added Tests for 4 services out of 9 (GC gives only 4working IPs)

--- Tests/ContainerTestsProd.ps1
+++ Tests/ContainerTestsProd.ps1
@@ -16,3 +16,60 @@ do {
 if (!$started) {
     exit 1
 }
+
+$count = 0
+do {
+    $count++
+    Write-Output "[$env:STAGE_NAME] Starting container [Attempt: $count]"
+    
+    $testStart = Invoke-WebRequest -Uri http://34.123.241.8:5003/index.html -UseBasicParsing
+    
+    if ($testStart.statuscode -eq '200') {
+        $started = $true
+    } else {
+        Start-Sleep -Seconds 10
+    }
+    
+} until ($started -or ($count -eq 3))
+
+if (!$started) {
+    exit 1
+}
+
+$count = 0
+do {
+    $count++
+    Write-Output "[$env:STAGE_NAME] Starting container [Attempt: $count]"
+    
+    $testStart = Invoke-WebRequest -Uri http://35.193.222.65:5009/index.html -UseBasicParsing
+    
+    if ($testStart.statuscode -eq '200') {
+        $started = $true
+    } else {
+        Start-Sleep -Seconds 10
+    }
+    
+} until ($started -or ($count -eq 3))
+
+if (!$started) {
+    exit 1
+}
+
+$count = 0
+do {
+    $count++
+    Write-Output "[$env:STAGE_NAME] Starting container [Attempt: $count]"
+    
+    $testStart = Invoke-WebRequest -Uri http://34.67.159.110:5007/index.html -UseBasicParsing
+    
+    if ($testStart.statuscode -eq '200') {
+        $started = $true
+    } else {
+        Start-Sleep -Seconds 10
+    }
+    
+} until ($started -or ($count -eq 3))
+
+if (!$started) {
+    exit 1
+}
